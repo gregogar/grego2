1abd6f479c700fc08277f39ab7a645db114b277f
grego <gregogar@ucm.es>
2026-09-14 18:03:49 +0200

    prueba
    Added an additional print statement for testing.

--- DAM2/PSP/prueba2/src/App.java
+++ DAM2/PSP/prueba2/src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Esto es un prueba");
         System.out.println("Esto es un prueba");
+        System.out.println("Esto es un prueba");
     }
 }
